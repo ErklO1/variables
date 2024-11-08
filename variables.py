6495995d4628db3d762c28036d616803685cb365
ErklO1 <vefremov1233@gmail.com>
2024-11-08 18:03:10 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,5 @@
+number_of_completed_HW_assignments=12
+number_of_hours_spent=1.5
+course_name='Python'
+time_per_assignment=number_of_hours_spent/number_of_completed_HW_assignments
+print('Курс: '+course_name+', всего задач: '+str(number_of_completed_HW_assignments)+', затрачено часов: '+str(number_of_hours_spent)+', среднее время выполнения: '+str(time_per_assignment)+'.')
